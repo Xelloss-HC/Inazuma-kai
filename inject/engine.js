d535591a25a288d0c1210b1d9a31a00e54dbb5cc
Xelloss-HC <xelloss.hc@gmail.com>
2016-08-11 03:54:10 +0800

    engine click use tap event

--- inject/engine.js
+++ inject/engine.js
@@ -44,7 +44,31 @@ window.Engine = {
 	},
 	click: function(selector) {
 		window.localStorage.setItem('button-selector', selector);
-		$(selector) && $(selector).trigger('tap');
+		var button = $(selector)
+		if (button) {
+			var e, o = $(document.documentElement).css('zoom'),
+				btninfo = {
+					x: 0x0,
+					y: 0x0,
+					w: 0x0,
+					h: 0x0
+				},
+				a = button.parents();
+			if (btninfo.w = button.innerWidth() * o, btninfo.h = button.innerHeight() * o, a.is(document.body))
+				for (var s = button; s[0x0] != document.body; s = s.parent()) e = s.position(), btninfo.y = btninfo.y + e.top * o, btninfo.x = btninfo.x + e.left * o;
+
+			tx = Math.round(btninfo.x + Math.random() * btninfo.w);
+			ty = Math.round(btninfo.y + Math.random() * btninfo.h);
+			tpe = new MouseEvent('tap', {
+				view: window,
+				bubbles: true,
+				clientX: tx,
+				clientY: ty,
+				cancelable: true
+			});
+
+			false != button.length && button[0].dispatchEvent(tpe);
+		}
 		this.debug('click:', selector);
 	},
 	eachWave: function(wave) {
